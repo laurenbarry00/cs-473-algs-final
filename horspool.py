d0f3bfa2a4347aaf85480357f890e2c3f01b1862
ajsewell <39346840+ajsewell@users.noreply.github.com>
2019-12-11 21:05:05 -0800

    Add files via upload

--- horspool.py
+++ horspool.py
@@ -1,4 +1,4 @@
-#Horspool algorithm based on pseudocode from pg. 261-262 of
+#Horspool algorithm based on pseudocode from pg. 261-262 of 
 #Introduction to Algorithm Design and Analysis by Levitin
 def ShiftTable(P):
     #P is the pattern
@@ -6,26 +6,25 @@ def ShiftTable(P):
     A = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G'
     , 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm',
     'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't',
-    'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '1', '2',
+    'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '0', '1', '2',
     '3', '4', '5', '6', '7', '8', '9']
-    m = len(P)
+    m = len(P) 
     T = {} #T is the table for the shifts
     for i in range(0, len(A)):
-        if A[i] not in P[0:m]: #last letter doesn't match so shift
+        if A[i] not in P[0:m - 1]: #last letter doesn't match so shift 
             #by entire pattern length
             T[A[i]] = m
         else:
-            if m - 1 - i > 0:
-                T[A[i]] = m - 1 - i
-            else:
-                T[A[i]] = 1
+            for x in range(0, m - 1):  #distance from rightmost x to last character
+                if P[x] == A[i]:
+                    T[A[i]] = m - x - 1
+                
 
     return T
 
 
 def HorspoolSearch(P, X):
-    count = 0
-
+    
     T = ShiftTable(P)
 
     m = len(P)
@@ -38,16 +37,19 @@ def HorspoolSearch(P, X):
 
         k = 0
 
-        while k < m  and P[m - 1 - k] == X[i - k]:
-            count = count + 1 #increment until length of pattern is reached
+        while k <= m  and P[m - 1 - k] == X[i - k]:
+ #increment until length of pattern is reached 
             #or a mismatch is found
             k = k + 1
 
         if k == m:
+            print("index of pattern is " , i - m + 1)
             return i - m + 1  #found a match
 
         else:
 
             i = i + T[X[i]]
-    print("Horspool: Not found")
+
     return -1
+
+
